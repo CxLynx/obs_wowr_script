a4ca1143d5427a069c97622384dddec880acd3a6
CLynx <cyberrr.lynx@gmail.com>
2024-11-22 11:12:53 +0100

    Kitchen sink commit with all my pre-VCS modifications. Most notably completely changed raid behaviour.

--- README.md
+++ README.md
@@ -3,13 +3,14 @@ My attempt at an OBS Plugin to start recordings based on World of Warcraft event
 
 # Installation
 1) Ensure Python is installed.
-2) In OBS Studio, under Tools > Scripts, in the `Python Settings` tab, update your 
+2) Ensure the Python file_read_backwards package is installed for the Python version to be used by OBS.
+3) In OBS Studio, under Tools > Scripts, in the `Python Settings` tab, update your 
 Python Install Path to the location of your Python installation. If you do this correctly, it will say
 something like "Loaded Python Version 3.12" below the path.
-3) Modify the script with your parameters
+4) Modify the script with your parameters
    1) At the top of the scripts under the # Constants section, update the `LOG_DIR` entry to be the path
    to your World of Warcraft installation. The double backslashes `\\` are intended, use two backslashes for
    each backslash in the path. 
-4) Add the script to your OBS Studio under Tools > Scripts in the `Scripts` tab. That's it!
-5) You can view what the script is doing by clicking the `Script Log` button. The script also logs things
+5) Add the script to your OBS Studio under Tools > Scripts in the `Scripts` tab. That's it!
+6) You can view what the script is doing by clicking the `Script Log` button. The script also logs things
 to your WoW Log directory in a `wowr_log.txt` file. 
--- obs_wowr.py
+++ obs_wowr.py
@@ -2,6 +2,7 @@ import glob
 import os
 import datetime
 import obspython as obs
+from time import sleep
 from file_read_backwards import FileReadBackwards
 
 # Constants
@@ -9,13 +10,13 @@ ENCOUNTER_START_TRIGGER = "ENCOUNTER_START"
 CHALLENGE_MODE_START_TRIGGER = "CHALLENGE_MODE_START"
 ENCOUNTER_END_TRIGGER = "ENCOUNTER_END"
 CHALLENGE_MODE_END_TRIGGER = "CHALLENGE_MODE_END"
-TIME_BACKSTOP = datetime.timedelta(seconds=10)
-LOG_DIR = "C:\\Games\\World of Warcraft\\_retail_\\Logs\\"
+TIME_BACKSTOP = datetime.timedelta(seconds=2)
+LOG_DIR = "C:\\Program Files (x86)\\World of Warcraft\\_retail_\\Logs\\"
 TIMESTAMP_FORMAT = "%m/%d/%Y %H:%M:%S"
 
 # Globals
 log_file = ""
-debug = True
+debug = False
 in_encounter = False
 in_dungeon = False
 
@@ -25,7 +26,7 @@ def update_combat_log_file():
   list_of_files = glob.glob(LOG_DIR + 'WoWCombatLog-*.txt')
   latest_file = max(list_of_files, key=os.path.getctime)
   log_file = latest_file
-  log_with_message(f"Latest log file: {latest_file}")
+  if debug: log_with_message(f"Latest log file: {latest_file}")
 
 
 def get_line_timestamp(line: str) -> datetime.datetime:
@@ -45,14 +46,13 @@ def first_run_log():
 
 
 def log_with_message(message: str):
-  log_entry = f"{datetime.datetime.now()} - {message}\n"
+  log_entry = f"{datetime.datetime.now()} - {message}"
   with open(LOG_DIR + "wowr_log.txt", "a") as file:
     file.write(log_entry)
   print(log_entry)
 
 
 def read_combat_log():
-  first_run_log()
   update_combat_log_file()
   with FileReadBackwards(log_file) as file:
     for line in file:
@@ -68,6 +68,15 @@ def read_combat_log():
 
 def change_combat_state(line: str) -> bool:
   global in_dungeon, in_encounter
+  """These hopefully prevent few-second recordings on key start (noticed combat log tends to go like START->END->START on key start)
+  Also implemented for raid with hunter/mage etc. boss resets in mind"""
+  if ENCOUNTER_END_TRIGGER in line and not in_encounter and not in_dungeon:
+    log_with_message("Detected ENCOUNTER_END, but recording wasn't active. Skipping older events")
+    return True
+  if CHALLENGE_MODE_END_TRIGGER in line and not in_dungeon:
+    log_with_message("Detected CHALLENGE_MODE_END, but recording wasn't active. Skipping older events")
+    return True
+
   if CHALLENGE_MODE_START_TRIGGER in line and not in_dungeon:
     log_with_message("CHALLENGE_MODE_START detected...starting M+ run...")
     log_with_message(line)
@@ -90,28 +99,48 @@ def change_combat_state(line: str) -> bool:
       log_with_message(line)
       in_encounter = True
       log_with_message("Changing Recording state to ON")
-      change_recording_state("start")
+      if not recording_active:
+        change_recording_state("start")
+      elif recording_active:
+        if recording_paused:
+          change_recording_state("start")
+          sleep(0.5) # IDK if this is needed, haven't tested without
+        add_chapter()
       return True
-
   if ENCOUNTER_END_TRIGGER in line and in_encounter:
     if not in_dungeon:
       log_with_message("Raid encounter end detected...ending raid encounter...")
       log_with_message(line)
       in_encounter = False
-      log_with_message("Changing Recording state to OFF")
-      change_recording_state("end")
-      return True
+      log_with_message("Changing Recording state to PAUSED")
 
+      change_recording_state("pause")
+      return True
   return False
 
 
 def change_recording_state(mode: str):
   if mode == "start":
-    if recording_active():
+    if recording_active() and not recording_paused():
       log_with_message("Recording already active...skipping...")
+    elif recording_active() and recording_paused():
+      log_with_message("Attempting to unpause recording...")
+      unpause_recording()
     else:
       log_with_message("Attempting to start recording...")
       start_recording()
+  elif mode == "split":
+    if mode == "split":
+      if recording_active():
+        log_with_message("Attempting to split recording...")
+        split_recording()
+      else:
+        log_with_message("Recording not active... can't split")
+  elif mode == "pause":
+    if mode == "pause":
+      if recording_active() and not recording_paused():
+        log_with_message("Attempting to pause recording...")
+        pause_recording()
   elif mode == "end":
     if recording_active():
       log_with_message("Attempting to stop recording...")
@@ -127,6 +156,9 @@ def write_debug_log_file(log_entry: str):
 def recording_active() -> bool:
   return obs.obs_frontend_recording_active()
 
+def recording_paused() -> bool:
+  return obs.obs_frontend_recording_paused()
+
 
 def start_recording():
   """Starts recording if recording is not active"""
@@ -139,7 +171,6 @@ def start_recording():
     else:
       log_with_message("Recording started...")
 
-
 def stop_recording():
   """Stops recording if recording is active"""
   if recording_active():
@@ -151,9 +182,57 @@ def stop_recording():
     else:
       log_with_message("Recording stopped...")
 
+def split_recording():
+  """Splits currently active recording into another file"""
+  if recording_active() and not recording_paused():
+    log_with_message("Splitting recording...")
+    try:
+      obs.obs_frontend_recording_split_file()
+    except Exception as e:
+      log_with_message(f"Failed to split recording. {e}")
+    else:
+      log_with_message("Recording split...")
+
+def add_chapter():
+  """Adds a chapter marker to the recording, requires recording to 'Hybrid MP4' and OBS Studio 31"""
+  if recording_active() and not recording_paused():
+    log_with_message("Adding a new chapter...")
+    try:
+      obs.obs_frontend_recording_add_chapter(None)
+    except Exception as e:
+      log_with_message(f"Failed to add chapter. {e}")
+    else:
+      log_with_message("Chapter added...")
+  elif recording_paused():
+    log_with_message("!!! add_chapter was called but the recording is still paused !!!")
+
+def pause_recording():
+  """Pauses the recording if it's active"""
+  if recording_active() and not recording_paused():
+    log_with_message("Pausing recording")
+    try:
+      obs.obs_frontend_recording_pause(True)
+    except Exception as e:
+      log_with_message(f"Failed to pause recording. {e}")
+    else:
+      log_with_message("Recording paused...")
+
+def unpause_recording():
+  """Unpauses the recording if it's active and paused"""
+  if recording_active() and recording_paused():
+    log_with_message("Unpausing recording")
+    try:
+      obs.obs_frontend_recording_pause(False)
+    except Exception as e:
+      log_with_message(f"Failed to unpause recording. {e}")
+    else:
+      log_with_message("Recording unpaused...")
+
 
 def script_description():
   return "Reads the World of Warcraft Combat Log to check for combat events to start and stop recording.\n\nBy Drathion"
 
+def script_load(settings):
+  first_run_log()
 
-obs.timer_add(read_combat_log, 3000)
+obs.timer_add(read_combat_log, 1000)
